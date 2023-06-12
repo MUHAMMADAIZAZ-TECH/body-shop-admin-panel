60f10e25784d8948b8c34d9abee9d7bbadf067e5
MUHAMMADAIZAZ-TECH <aizaznadeem174@gmail.com>
2023-06-12 10:41:46 +0500

    services list

--- src/container/services/serviceslist/Index.js
+++ src/container/services/serviceslist/Index.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Row, Col, Table, Spin } from 'antd';
+import React, { useEffect, useState,useRef } from 'react';
+import { Row, Col, Table, Spin,Avatar,Tag,Input,Space } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
+import { SearchOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import Highlighter from 'react-highlight-words';
 import FeatherIcon from 'feather-icons-react';
 import { RecordViewWrapper } from './Style';
 import { Main, TableWrapper } from '../../styled';
@@ -16,16 +19,34 @@ import {
 } from '../../../redux/crud/axios/actionCreator';
 import { getServices } from '../../../redux/services/servicesSlice';
 
+const avatarStyle = {
+  borderRadius: '4px', // Adjust the border radius as per your preference
+  width: '60px', // Adjust the width and height as per your preference
+  height: '60px',
+  lineHeight: '100px', // Vertically center the content
+  textAlign: 'center', // Horizontally center the content
+};
 const ViewPage = () => {
   const dispatch = useDispatch();
   const { crud, isLoading,servicesStates } = useSelector(state => {
     return {
       crud: state.AxiosCrud.data,
       isLoading: state.AxiosCrud.loading,
-      servicesStates: state.servicesStates
+      servicesStates: state.servicesStates,
     };
   });
-
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
   const [state, setState] = useState({
     selectedRowKeys: [],
   });
@@ -52,122 +73,186 @@ const ViewPage = () => {
     }
     return false;
   };
-
+  const handleEdit = (salon) => {
+    console.log(salon)
+  }
   const onHandleSearch = e => {
     dispatch(axiosDataSearch(e.target.value, crud));
   };
 
-  if (crud.length)
-    crud.map((person, key) => {
-      const { id, name, email, company, position, join, status, city, country, image } = person;
-      return dataSource.push({
-        key: key + 1,
-        name: (
-          <div className="record-img align-center-v">
-            <img
-              src={
-                image ? process.env.REACT_APP_BASE_URL + image : require('../../../static/img/avatar/profileImage.png')
-              }
-              alt={id}
-            />
-            <span>
-              <span>{name}</span>
-              <span className="record-location">{city && country ? `${city},${country}` : ''}</span>
-            </span>
-          </div>
-        ),
-        email,
-        company,
-        position,
-        jdate: join,
-        status: <span className={`status ${status}`}>{status}</span>,
-        action: (
-          <div className="table-actions">
-            <Link className="edit" to={`/salon/salon/edit/${id}`}>
-              <FeatherIcon icon="edit" size={14} />
-            </Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link className="delete" onClick={() => handleDelete(id)} to="#">
-              <FeatherIcon icon="trash-2" size={14} />
-            </Link>
-          </div>
-        ),
-      });
+  console.log(servicesStates?.services)
+  if (servicesStates?.services?.length)
+  servicesStates?.services?.map((salon, key) => {
+    const { id, image, name, salon_name, price, category_name, is_available, updated_at } = salon;
+    return dataSource.push({
+      key: key + 1,
+      image: (image && <Avatar style={avatarStyle} src={image} size={60}/>),
+      name,
+      salon_name,
+      price:<span>{price} $</span>,
+      category_name,
+      is_available: <Tag className='complete'>{is_available === 1 ? 'yes' : "no"}</Tag>,
+      updated_at,
+      action: (
+        <div className="table-actions">
+          <Link className="edit" to={`/admin/salon/edit/${id}`} onClick={() => handleEdit(salon)}>
+            <FeatherIcon icon="edit" size={14} />
+          </Link>
+          &nbsp;&nbsp;&nbsp;
+          <Link className="delete" onClick={() => handleDelete(id)} to="#">
+            <FeatherIcon icon="trash-2" size={14} />
+          </Link>
+        </div>
+      ),
+      salon
     });
+  });
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+      // onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
 
-  // const columns = [
-  //   {
-  //     title: 'Name',
-  //     dataIndex: 'name',
-  //     key: 'name',
-  //   },
-  //   {
-  //     title: 'Email',
-  //     dataIndex: 'email',
-  //     key: 'email',
-  //   },
-  //   {
-  //     title: 'Company',
-  //     dataIndex: 'company',
-  //     key: 'company',
-  //   },
-  //   {
-  //     title: 'Position',
-  //     dataIndex: 'position',
-  //     key: 'position',
-  //   },
-  //   {
-  //     title: 'Status',
-  //     dataIndex: 'status',
-  //     key: 'status',
-  //   },
-  //   {
-  //     title: 'Joining Date',
-  //     dataIndex: 'jdate',
-  //     key: 'jdate',
-  //   },
-  //   {
-  //     title: 'Actions',
-  //     dataIndex: 'action',
-  //     key: 'action',
-  //     width: '90px',
-  //   },
-  // ];
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="primary"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="primary"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
   const columns = [
     {
       title: 'Image',
       dataIndex: 'image',
       key: 'image',
+      sorter: (a, b) => a.image.length - b.image.length,
+      sortDirections: ['descend', 'ascend'],
     },
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ['descend', 'ascend'],
+      ...getColumnSearchProps('name'),
     },
     {
       title: 'Salon',
       dataIndex: 'salon_name',
       key: 'salon_name',
+      sorter: (a, b) => a.salon_name.length - b.salon_name.length,
+      sortDirections: ['descend', 'ascend'],
     },
     {
       title: 'Price',
       dataIndex: 'price',
       key: 'price',
+      sorter: (a, b) => a.price.length - b.price.length,
+      sortDirections: ['descend', 'ascend'],
     },
     {
       title: 'Category Name',
       dataIndex: 'category_name',
       key: 'category_name',
+      sorter: (a, b) => a.category_name.length - b.category_name.length,
+      sortDirections: ['descend', 'ascend'],
     },
     {
       title: 'Available',
       dataIndex: 'is_available',
       key: 'is_available',
+      sorter: (a, b) => a.is_available.length - b.is_available.length,
+      sortDirections: ['descend', 'ascend'],
     },
     {
       title: 'Updated At',
       dataIndex: 'updated_at',
       key: 'updated_at',
+      sorter: (a, b) => a.updated_at.length - b.updated_at.length,
+      sortDirections: ['descend', 'ascend'],
+      render: text => moment(text).fromNow(),
     },
     {
       title: 'Actions',
@@ -179,7 +264,6 @@ const ViewPage = () => {
   const onSelectChange = selectedRowKey => {
     setState({ ...state, selectedRowKeys: selectedRowKey });
   };
-  console.log(servicesStates)
   const rowSelection = {
     selectedRowKeys,
     onChange: onSelectChange,
@@ -224,7 +308,7 @@ const ViewPage = () => {
                     <Table
                       rowSelection={rowSelection}
                       pagination={{ pageSize: 10, showSizeChanger: true }}
-                      dataSource={servicesStates?.services}
+                      dataSource={dataSource}
                       columns={columns}
                     />
                   </TableWrapper>
